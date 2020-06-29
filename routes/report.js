49beda10b68219843d13d72fdabe6d86ea93b572
Hyejeong Kang <s2018w01@gmail.com>
2020-06-29 23:11:16 +0900

    setting report

---
+++ routes/report.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/list', function(req, res, next) {
+    res.render('report_list');
+});
+
+router.post('/list', function(req, res, next) {
+
+});
+
+router.get('/add', function(req, res, next) {
+    res.render('report');
+});
+
+router.post('/add', function(req, res, next) {
+
+});
+
+module.exports = router;
