ec6366358b551b0846cd57c4479dc19ea8e07069
Hyejeong Kang <s2018w01@gmail.com>
2020-06-30 00:57:45 +0900

    modify structure

--- routes/report.js
+++ routes/report.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('report_list');
 });
 
 router.get('/list', function(req, res, next) {
