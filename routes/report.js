4af1782d8a6b004846143ad439baa2bd236f6ef0
Hyejeong Kang <s2018w01@gmail.com>
2020-06-30 08:38:28 +0900

    modify report

--- routes/report.js
+++ routes/report.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var getConn = require('../db/connection')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('report_list');
+    getConn((err, db) => {
+        if (err) throw err;
+        var user = db.db('local');
+        var report = user.collection('place');
+
+        report.find().toArray((err, result) => {
+            console.log(result);
+            res.send({'ok': true});
+        });
+    });
+
+
 });
 
 router.get('/list', function(req, res, next) {
