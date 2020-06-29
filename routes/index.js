899b3c72b85378defdadb24804f7675ff2faf055
yerin <ghddnjf2901@gmail.com>
2020-06-30 08:11:31 +0900

    main 고장 신고 현황 테스트

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var getConn = require('../db/connection');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  getConn((err, db) => {
+    if (err) throw err;
+    var user = db.db('local');
+    var report = user.collection('report');
+
+    report
+      .find()
+      .sort({ amount: 1, date: -1 })
+      .limit(5)
+      .toArray((err, docs) => {
+        if (err) throw err;
+        if (docs.length > 0) {
+          console.log('docs : ' + docs);
+          console.dir(docs);
+          res.render('index', { docs: docs });
+        } else {
+          console.log('projects 리스트 없음');
+          res.render('index', { docs: [] });
+        }
+      });
+  });
 });
 
 module.exports = router;
