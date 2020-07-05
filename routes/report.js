12cc8358558206a1dbcfe57b708ec97d359120ad
Hyejeong Kang <s2018w01@gmail.com>
2020-07-05 13:36:45 +0900

    Modify the report list to be visible

--- routes/report.js
+++ routes/report.js
@@ -4,22 +4,37 @@ var getConn = require('../db/connection')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
     getConn((err, db) => {
         if (err) throw err;
         var user = db.db('local');
-        var report = user.collection('place');
+        var report = user.collection('report');
 
         report.find().toArray((err, result) => {
             console.log(result);
-            res.send({'ok': true});
+            res.render('report_list', { result: result });
         });
     });
-
-
 });
 
 router.get('/list', function(req, res, next) {
-    res.render('report_list');
+    getConn((err, db) => {
+        if (err) throw err;
+        var user = db.db('local');
+        var report = user.collection('report');
+
+        report.find().toArray((err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                console.log(result);
+                res.render('report_list', {result: result});
+            } else {
+                console.log('report 리스트 없음');
+                console.log(result);
+                res.render('report_list', {result: []});
+            }
+        });
+    });
 });
 
 router.post('/list', function(req, res, next) {
@@ -34,11 +49,11 @@ router.post('/add', function(req, res, next) {
 
 });
 
-router.get('/read', function(req, res, next) {
+router.get('/view', function(req, res, next) {
     res.render('read_report');
 });
 
-router.post('/read', function(req, res, next) {
+router.post('/view', function(req, res, next) {
 
 });
 
