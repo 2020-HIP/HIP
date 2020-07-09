2aeace751cf9880f6a14ae91e642e4fb42a32f72
Hyejeong Kang <s2018w01@gmail.com>
2020-07-09 11:42:28 +0900

    report/update

--- routes/report.js
+++ routes/report.js
@@ -327,21 +327,25 @@ router.post('/add', function (req, res, next) {
 
 
 router.post('/update', function (req, res, next) {
+  let place_id = req.query.place_id || req.body.place_id;
 
   getConn((err, db) => {
     if (err) throw err;
-    var user = db.db('hip');
-    var report = user.collection('report');
+    let user = db.db('hip');
+    let place = user.collection('report');
 
-    report.find({}).toArray((err, reports) => {
+    place.find({ place_id: place_id }).toArray((err, result) => {
+      console.log('별별별');
+      console.log(result);
+
       if (err) throw err;
-      let last_report = reports.length - 1;
-      if (reports.length - 1 < 0) {
-        req.num = 0;
+      if (result.length > 0) {
+        req.num = result[0].num;
+        next();
       } else {
-        req.num = 21;
+        req.num = -1;
+        next();
       }
-      next();
     });
   });
 });
@@ -349,43 +353,33 @@ router.post('/update', function (req, res, next) {
 router.post('/update', function (req, res, next) {
   let num = req.num;
   let place_id = req.query.place_id || req.body.place_id;
-  let computer_id = req.query.computer_id || req.body.computer_id;
   let wrong_report = req.query.wrong_report || req.body.wrong_report;
   let report_title = req.query.report_title || req.body.report_title;
   let report_content = req.query.report_content || req.body.report_content;
   let password = req.query.password || req.body.password;
-  let writer_name = req.query.writer_name || req.body.writer_name;
-  let date = formatDate(new Date());
   let solution = 0;
 
-  console.log('HI');
-  console.log(typeof num);
-
-  console.log(`num(${place_id}), lace_id(${place_id}), computer_id(${computer_id}), wrong_report(${wrong_report}) ,
-    report_title(${report_title}), report_content(${report_content}), password(${password}), writer_name(${writer_name}),
-    date(${date}), solution=(${solution})`);
+  console.log('별별별별별 : ' + num);
 
   getConn((err, db) => {
     if (err) throw err;
     var user = db.db('hip');
     var report = user.collection('report');
 
-    report.insertOne(
-        {
-          num: parseInt(num),
-          place_id: place_id,
-          computer_id: computer_id,
-          wrong_report: wrong_report,
-          report_title: report_title,
-          report_content: report_content,
-          password: password,
-          writer_name: writer_name,
-          date: date,
-          solution: solution,
+    report.updateOne(
+        { num: parseInt(num) },
+        { $set:
+              {
+                wrong_report: wrong_report,
+                report_title: report_title,
+                report_content: report_content,
+                password: password,
+                solution: solution
+              }
         },
         (err, result) => {
           if (err) throw err;
-          res.redirect('list?place_id=' + place_id);
+          res.redirect(`/report/read/${place_id}/${num}`);
         }
     );
   });
